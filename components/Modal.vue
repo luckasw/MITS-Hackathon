976fcab77f130a9afdd6da040c2275f57631dbd7
kevinkliimask <kevinkliimask@gmail.com>
2023-12-09 01:27:27 +0200

    Start with admin view

---
+++ components/Modal.vue
@@ -0,0 +1,10 @@
+<script></script>
+
+<template>
+    <div>
+        <div>
+            <slot />
+            <button>Katkesta</button>
+        </div>
+    </div>
+</template>
---
+++ pages/admin.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from "vue-qrcode-reader";
+import Modal from "../components/Modal.vue";
+
+export default defineComponent({
+  name: "Registration",
+  components: {
+    QrcodeCapture,
+    QrcodeDropZone,
+    QrcodeStream,
+  },
+  methods: {
+    onDetect(detectedCodes) {
+      console.log(detectedCodes);
+      this.showModal = true;
+    },
+  },
+  data() {
+    return {
+      showModal: false,
+    }
+  }
+});
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <qrcode-stream @detect="onDetect" />
+    </div>
+  </div>
+  <Modal v-if="showModal">
+    <h1>Registreeri tops</h1>
+  </Modal>
+</template>
+
+<style scoped></style>
