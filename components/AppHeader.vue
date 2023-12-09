8d87922645e8d9639ed21c0bc124dd9d352033a6
Kevin Kliimask <kevin.kliimask@ut.ee>
2023-12-09 14:41:21 +0200

    Minor changes

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-const appConfig = useAppConfig()
+const appConfig = useAppConfig();
 </script>
 
 <template>
   <div class="header">
     <h1>Gringo</h1>
-    <div class="navbar">
-      <nav>
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="/about">About</NuxtLink>
-        <NuxtLink to="/login">Login</NuxtLink>
-        <NuxtLink to="/register">Register</NuxtLink>
-      </nav>
-    </div>
+    <nav>
+      <NuxtLink to="/">Esileht</NuxtLink>
+      <NuxtLink to="/about">Profiil</NuxtLink>
+      <NuxtLink to="/login">Log in</NuxtLink>
+      <NuxtLink to="/register">Register</NuxtLink>
+    </nav>
   </div>
 </template>
 
 <style scoped>
 .header {
   background-color: #0079ff;
-  padding: 20px;
+  padding: 2rem;
   text-align: center;
 }
-.navbar {
-  padding: 20px;
+nav {
+  margin-top: 1rem;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
